a4d71005ed6b71783392b8c9be4062bce53ecc63
Jonathan Duggan <jonathan.duggan@sonalake.com>
2017-08-01 15:42:17 +0100

    refactor js imports

---
+++ js/latest-news.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+    $.get("https://graph.facebook.com/v2.10/lcctennisclub?fields=feed.limit(20){type,story,message,picture,icon,link,name,created_time}&access_token=668537306685838%7C4vvOZgjzjmdW-sw8C3TMX6QCyKI",
+        function(data, status){
+            var articleCount = 0;
+            var mainArticleAdded = false;
+
+            for(i=0; articleCount<5; i++){
+                if(!mainArticleAdded){
+                    if(addArticleToDiv(data.feed.data[0], 'main-article-template', 'latest-main-article-container')){
+                        mainArticleAdded = true;
+                        articleCount++;
+                    }
+                }
+                else if(addArticleToDiv(data.feed.data[i], 'sub-article-template', 'latest-sub-articles-container')){
+                    articleCount++;
+                }
+            }
+        }
+    );
+});
+
+function addArticleToDiv(article, templateId, containerId){
+    var context = {};
+    context.content = article.message;
+    context.picture = article.picture;
+
+    if(article.type === 'status'){
+        return false;
+    }
+    else if(article.type === 'event'){
+        context.title = article.name;
+    }
+    else{
+        context.title = "LCC News";
+    }
+
+    context.time = moment(article.created_time).fromNow();
+    var source   = $("#" + templateId).html();
+    var template = Handlebars.compile(source);
+    var html    = template(context);
+    $('#' + containerId).append(html);
+
+    return true;
+}
