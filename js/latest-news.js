dc1e48b274df3f3d66ec69fd589d061453a385ed
Jonathan Duggan <jduggan83@gmail.com>
2018-02-02 16:28:06 +0000

    removed hardcoded LCC Open notice

--- js/latest-news.js
+++ js/latest-news.js
@@ -2,19 +2,9 @@ $( document ).ready(function() {
     $.get("https://graph.facebook.com/v2.10/lcctennisclub/posts?fields=description,link,created_time,message,story,name,type&access_token=668537306685838%7C4vvOZgjzjmdW-sw8C3TMX6QCyKI",
         function(data, status){
 
-            for(i=0; i<3; i++){
+            for(i=0; i<4; i++){
                 addEventToDiv(data.data[i], i)
             }
-
-            var lccOpen = {
-                content: "The 2018 LCC Open Tournament is running from the 20th to the 27th of April. Online entry will be available closer to the date. Put the dates in your diary now so you don’t forget to enter!",
-                title: "LCC Open 2018",
-                type: "status"
-            }
-            var source   = $("#news-template").html();
-            var template = Handlebars.compile(source);
-            var html    = template(lccOpen);
-            $('#news-3').append(html);
         }
     );
 });
